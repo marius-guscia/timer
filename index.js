e746ba23aaae9c6b0aa4c992f1fe53df5cc706d8
Marius Guscia <marius.guscia@gmail.com>
2021-05-13 16:50:02 +0300

    Added basic elements & feedback

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-console.log('Hi there.')
+class Timer {
+    constructor(durationInput, startButton, pauseButton) {
+        this.durationInput = durationInput;
+        this.startButton = startButton;
+        this.pauseButton = pauseButton;
+
+        this.startButton.addEventListener('click', this.start);
+        this.pauseButton.addEventListener('click', this.pause);
+    }
+    start() {
+        console.log('Time to start the timer.')
+    }
+    pause() {
+        console.log('Time to pause the timer.')
+    }
+    // onDurationChange() {
+
+    // }
+    // tick() {
+
+    // }
+}
+
+const durationInput = document.querySelector('#duration');
+const startButon = document.querySelector('#start')
+const pauseButon = document.querySelector('#pause')
+
+const timer = new Timer(durationInput, startButon, pauseButon)
