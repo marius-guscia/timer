00735a0ece81d157ac5bdc8ad9a4ad7928428203
Marius Guscia <marius.guscia@gmail.com>
2021-05-13 17:46:42 +0300

    Added set/get methods, expanded tick method.

--- index.js
+++ index.js
@@ -7,18 +7,30 @@ class Timer {
         this.startButton.addEventListener('click', this.start);
         this.pauseButton.addEventListener('click', this.pause);
     }
+
     start = () => {
         this.tick();
         this.intervalId = setInterval(this.tick, 1000)
     }
+
     pause = () => {
         clearInterval(this.intervalId);
     }
-    // onDurationChange() {
 
-    // }
     tick = () => {
-        console.log('tick')
+        if (this.timeRemaining <= 0) {
+            this.pause()
+        } else {
+            this.timeRemaining = this.timeRemaining - 1;
+        }
+    }
+
+    get timeRemaining() {
+        return parseFloat(this.durationInput.value)
+    }
+
+    set timeRemaining(time) {
+        this.durationInput.value = time;
     }
 }
 
