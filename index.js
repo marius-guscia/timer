3c1c504e9a704b84308cc9921423a325148f8d56
Marius Guscia <marius.guscia@gmail.com>
2021-05-14 10:20:49 +0300

    Added onStart, onTick & onComplete callbacks

--- index.js
+++ index.js
@@ -1,41 +1,65 @@
 class Timer {
-    constructor(durationInput, startButton, pauseButton) {
+    constructor(durationInput, startButton, pauseButton, callbacks) {
         this.durationInput = durationInput;
         this.startButton = startButton;
         this.pauseButton = pauseButton;
+        if (callbacks) {
+            this.onStart = callbacks.onStart;
+            this.onTick = callbacks.onTick;
+            this.onComplete = callbacks.onComplete;
+        };
 
         this.startButton.addEventListener('click', this.start);
         this.pauseButton.addEventListener('click', this.pause);
-    }
+    };
 
     start = () => {
+        if (this.onStart) {
+            this.onStart();
+        };
         this.tick();
-        this.intervalId = setInterval(this.tick, 1000)
-    }
+        this.intervalId = setInterval(this.tick, 1000);
+    };
 
     pause = () => {
         clearInterval(this.intervalId);
-    }
+    };
 
     tick = () => {
         if (this.timeRemaining <= 0) {
-            this.pause()
+            this.pause();
+            if (this.onComplete) {
+                this.onComplete();
+            };
         } else {
             this.timeRemaining = this.timeRemaining - 1;
+            if (this.onTick) {
+                this.onTick();
+            };
         }
-    }
+    };
 
     get timeRemaining() {
         return parseFloat(this.durationInput.value)
-    }
+    };
 
     set timeRemaining(time) {
         this.durationInput.value = time;
-    }
+    };
 }
 
 const durationInput = document.querySelector('#duration');
-const startButon = document.querySelector('#start')
-const pauseButon = document.querySelector('#pause')
+const startButton = document.querySelector('#start');
+const pauseButton = document.querySelector('#pause');
 
-const timer = new Timer(durationInput, startButon, pauseButon)
+const timer = new Timer(durationInput, startButton, pauseButton, {
+    onStart() {
+        console.log('timer started.')
+    },
+    onTick() {
+        console.log('time has ticked.')
+    },
+    onComplete() {
+        console.log('timer is complete.')
+    }
+});
